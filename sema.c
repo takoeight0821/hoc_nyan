3f9d32eb4065891f45655775936a53daf45d7ca5
Yuya Kono <takohati0821@gmail.com>
2019-06-24 20:21:31 +0900

    walkを追加

--- sema.c
+++ sema.c
@@ -1,5 +1,12 @@
 #include "hoc.h"
 
-void sema(Program* prog) {
+Node* walk(Vector* params, Node* node) {
+  return node;
+}
 
+void sema(Program* prog) {
+  for (size_t i = 0; i < prog->funcs->length; i++) {
+    Function* fn = prog->funcs->ptr[i];
+    fn->body = walk(fn->params, fn->body);
+  }
 }
