83b16f612c6a0b54201d219f95a9c7d850e3d402
Yuya Kono <takohati0821@gmail.com>
2019-08-03 01:26:19 +0900

    examples/fib.cにswitch-case文の例を追加

--- examples/fib.c
+++ examples/fib.c
@@ -1,13 +1,22 @@
-int fib(int n) {
+int fib_0(int n) {
   if (n == 0) {
     return 1;
   } else if (n == 1) {
     return 1;
   } else {
-    return fib(n - 1) + fib(n - 2);
+    return fib_0(n - 1) + fib_0(n - 2);
   }
 }
 
+int fib_1(int n) {
+  switch (n) {
+  case 0:
+  case 1:
+    return 1;
+  default:
+    return fib_1(n - 1) + fib_1(n - 2);
+  }
+}
 int main() {
-  return fib(10);
+  return fib_0(10) == fib_1(10);
 }
