196583b722998ac3778a0fec1a00384f033b6ce1
Yuya Kono <takohati0821@gmail.com>
2019-08-05 10:40:11 +0900

    改行を追加

--- token.c
+++ token.c
@@ -87,7 +87,8 @@ static char* symbols[] = {
   ":", ";", ",", ".",
   "auto", "break", "case", "char", "const", "continue", "default",
   "do", "double", "else", "enum", "extern", "float", "for", "goto",
-  "if", "inline", "int", "long", "register", "restrict", "return", "short", "sizeof", "static", "struct", "switch", "typedef",
+  "if", "inline", "int", "long", "register", "restrict", "return",
+  "short", "sizeof", "static", "struct", "switch", "typedef",
   "union", "unsigned", "void", "volatile", "while",
   "_Alignas", "_Alignof", "_Atomic", "_Bool", "_Complex", "_Generic",
   "_Imaginary", "_Noreturn", "_Static_assert", "_Thread_local"
