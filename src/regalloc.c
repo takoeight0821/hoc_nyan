19071694aef9d4cd7a9028b1d17ddaf62ac8c09b
Yuya Kono <takohati0821@gmail.com>
2019-08-31 23:51:31 +0900

    create src/regalloc.c

---
+++ src/regalloc.c
@@ -0,0 +1,11 @@
+#include "hoc.h"
+
+static Vector* collect_regs(IFunc* func) {
+  for (int i = 0; i < func->params->length; i++) {
+    IReg* param = func->params->ptr[i];
+  }
+}
+
+void alloc_regs(IProgram* prog) {
+  
+}
